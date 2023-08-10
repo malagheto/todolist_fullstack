8116f59dfd64b0615f5b4e02ce2594b1407a5310
Kawe da Cruz <kawecruzg@gmail.com>
2023-08-10 00:06:37 -0300

    add backend rep

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => { response.status(200).send('Olá, Mundo')})
+
+app.listen(3333, () => console.log('Server is running on port 3333.'))
+
+
