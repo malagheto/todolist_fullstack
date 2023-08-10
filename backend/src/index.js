8aea0f79ced12a2aa64868ac4b8a1908151b029b
Kawe da Cruz <kawecruzg@gmail.com>
2023-08-10 11:04:05 -0300

    add connections files

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (request, response) => { response.status(200).send('Olá, Mundo')})
-
-app.listen(3333, () => console.log('Server is running on port 3333.'))
-
-
